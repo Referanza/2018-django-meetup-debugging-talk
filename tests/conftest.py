5a79c67969199e9faad0a3c93ce09b8a6b4d1233
Max Nordlund gmail <max.nordlund@gmail.com>
2018-02-06 14:46:15 +0100

    Add bpython
    This includes a small helper for pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,40 @@
 import pytest
+import sys
+import os
+from bpdb.debugger import BPdb
+
+std = {
+    'in': None,
+    'out': None
+}
+
+class PytestBPdb(BPdb):
+    @property
+    def set_trace(self):
+        sys.stdout = std['out']
+        sys.__stdout__ = std['out']
+        return super(BPdb, self).set_trace
+
+@pytest.fixture
+def bpdb():
+    """
+    An :py:class:`bpdb.debugger.BPdb`
+
+    Bypasses stdio capture, but this only works for capture sys or no
+    """
+    fdin = os.dup(0)
+    std['in'] = os.fdopen(fdin, mode='r')
+    sys.stdin = std['in']
+    sys.__stdin__ = std['in']
+
+    fdout = os.dup(1)
+    std['out'] = os.fdopen(fdout, mode='w')
+    sys.stdout = std['out']
+    sys.__stdout__ = std['out']
+
+    try:
+        yield PytestBPdb(stdin=std['in'], stdout=std['out'])
+    finally:
+        os.dup2(fdin, 0)
+        os.dup2(fdout, 1)
+
